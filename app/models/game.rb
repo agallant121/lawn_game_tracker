dc2efb2c0fca3a738b6e9b801d8fd72df02a704c
Alex Gallant <agallant1201@gmail.com>
2025-05-05 10:36:50 -0400

    Add validations and associations to game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,7 @@
 class Game < ApplicationRecord
+  has_many :rankings, dependent: :destroy
+  has_many :players, through: :rankings
+
+  validates :name, presence: true
+  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
 end
