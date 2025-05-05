f0fb8d2129ff60763b6def2e3fc673bc0f479271
Alex Gallant <agallant1201@gmail.com>
2025-05-05 10:37:45 -0400

    Remove rating validation

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,5 +3,4 @@ class Game < ApplicationRecord
   has_many :players, through: :rankings
 
   validates :name, presence: true
-  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
 end
