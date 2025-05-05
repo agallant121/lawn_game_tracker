a134435446d9caa00f75d4bc309f9b275ed8f56a
Alex Gallant <agallant1201@gmail.com>
2025-05-05 13:19:42 -0400

    Add uniqueness scope for player names

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,9 +2,14 @@ class Player < ApplicationRecord
   has_many :rankings, dependent: :destroy
   has_many :games, through: :rankings
 
-  validates :first_name, :last_name, presence: true
+  validates :first_name, :last_name, presence: true 
+  validates :first_name, uniqueness: { scope: :last_name }
 
   enum role: { player: 0, admin: 1 }
 
   accepts_nested_attributes_for :rankings
+
+  def name
+    "#{first_name} #{last_name}"
+  end
 end
