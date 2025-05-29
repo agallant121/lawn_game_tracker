f1c7c27535d18171ab68f57602cd6b6710ffe6c5
Alex Gallant <agallant1201@gmail.com>
2025-05-29 10:56:31 -0400

    Added teams

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_05_144320) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_12_171416) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_catalog.plpgsql"
+  enable_extension "plpgsql"
 
   create_table "games", force: :cascade do |t|
     t.string "name"
@@ -38,6 +38,17 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_05_144320) do
     t.index ["player_id"], name: "index_rankings_on_player_id"
   end
 
+  create_table "teams", force: :cascade do |t|
+    t.bigint "players_id", null: false
+    t.bigint "games_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["games_id"], name: "index_teams_on_games_id"
+    t.index ["players_id"], name: "index_teams_on_players_id"
+  end
+
   add_foreign_key "rankings", "games"
   add_foreign_key "rankings", "players"
+  add_foreign_key "teams", "games", column: "games_id"
+  add_foreign_key "teams", "players", column: "players_id"
 end
